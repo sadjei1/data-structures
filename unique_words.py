68619b9ae970102b639f5e69925d31ec8715067c
sadjei1 <samuelkyeremeh19@yahoo.com>
2025-09-03 05:33:55 -0600

    More code

--- unique_words.py
+++ unique_words.py
@@ -1,1 +1,13 @@
-print("Hello World")
+
+def unique_words(word):
+    stack = []
+    for character in word:
+        if stack and stack[-1] == character:
+            stack.pop()
+        else:
+            stack.append(character)
+    return stack if stack else "Empty String"
+
+print(unique_words("aab"))
+
+
